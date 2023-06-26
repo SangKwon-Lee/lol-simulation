766b2ed4236b1a3e763ce3e28cae9323360c1ac6
SangKwon-Lee <sangkwon.lee@pxd.co.kr>
2023-06-26 14:08:29 +0900

    fix:키워드수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,7 +35,7 @@ const MetaTag = {
   'og:site_name': 'LOL Simulation',
   'og:image': '/images/hextech_chest.png',
   keywords:
-    'LOL Simulation, lol, simultation,롤 시뮬레이션, 시뮬레이션, 게임 시뮬레이션, 시뮬레이터, 롤 시뮬레이터, 상자깡, 상자, 마법공학 상자, 마법공학, 명품상자, 주머니, 토큰, 롤 상자깡, 라구, 롤, 리그오브레전드, 열쇠, 롤 스킨, 주황정수, 라구깡, 신스킨, 롤 신스킨, 먹그림자라구, 먹그림자라구깡, 롤 패스, kda, 프레스티지, 프레스티지 신스킨, 롤 스킨 목록'
+    'LOL Simulation, lol, simultation,롤 시뮬레이션, 롤 상자깡 시뮬레이션, 롤 상자깡,시뮬레이션, 게임 시뮬레이션, 시뮬레이터, 롤 시뮬레이터, 상자깡, 상자, 마법공학 상자, 마법공학, 명품상자, 주머니, 토큰, 롤 상자깡, 라구, 롤, 리그오브레전드, 열쇠, 롤 스킨, 주황정수, 라구깡, 신스킨, 롤 신스킨, 먹그림자라구, 먹그림자라구깡, 롤 패스, kda, 프레스티지, 프레스티지 신스킨, 롤 스킨 목록'
 };
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
